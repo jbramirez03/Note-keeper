789ac2ee8750751d9adc1753c9990c253c8c5f38
temptado <jason1287712@gmail.com>
2021-08-17 22:57:57 -0400

    started on adding express middleware in server.js file

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.port || 3001;
+
+const app = express();
+app.use(express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
